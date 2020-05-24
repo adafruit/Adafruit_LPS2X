810ae02579205e66f895e80c8332e245f630ae61
lady ada <limor@ladyada.net>
2020-05-23 22:40:54 -0400

    fix lps25 example

--- examples/adafruit_lps25_test/adafruit_lps25_test.ino
+++ examples/adafruit_lps25_test/adafruit_lps25_test.ino
@@ -10,31 +10,31 @@
 #define LPS_MISO 12
 #define LPS_MOSI 11
 
-Adafruit_LPS2X lps;
+Adafruit_LPS25 lps;
 
 void setup(void) {
   Serial.begin(115200);
   while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
 
-  Serial.println("Adafruit LPS2X test!");
+  Serial.println("Adafruit LPS25 test!");
 
   // Try to initialize!
   if (!lps.begin_I2C()) {
   //if (!lps.begin_SPI(LPS_CS)) {
   //if (!lps.begin_SPI(LPS_CS, LPS_SCK, LPS_MISO, LPS_MOSI)) {
-    Serial.println("Failed to find LPS2X chip");
+    Serial.println("Failed to find LPS25 chip");
     while (1) { delay(10); }
   }
-  Serial.println("LPS2X Found!");
+  Serial.println("LPS25 Found!");
 
-//  lps.setDataRate(LPS2X_RATE_12_5_HZ);
+//  lps.setDataRate(LPS25_RATE_12_5_HZ);
   Serial.print("Data rate set to: ");
   switch (lps.getDataRate()) {
-    case LPS2X_RATE_ONE_SHOT: Serial.println("One Shot"); break;
-    case LPS2X_RATE_1_HZ: Serial.println("1 Hz"); break;
-    case LPS2X_RATE_7_HZ: Serial.println("7 Hz"); break;
-    case LPS2X_RATE_12_5_HZ: Serial.println("12.5 Hz"); break;
-    case LPS2X_RATE_25_HZ: Serial.println("25 Hz"); break;
+    case LPS25_RATE_ONE_SHOT: Serial.println("One Shot"); break;
+    case LPS25_RATE_1_HZ: Serial.println("1 Hz"); break;
+    case LPS25_RATE_7_HZ: Serial.println("7 Hz"); break;
+    case LPS25_RATE_12_5_HZ: Serial.println("12.5 Hz"); break;
+    case LPS25_RATE_25_HZ: Serial.println("25 Hz"); break;
 
   }
 }
