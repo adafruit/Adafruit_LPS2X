3708962754307d70a6b503c7446d748dda3b3f03
Doxygen CI <travis@travis-ci.org>
2020-06-03 22:06:46 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_a.js
+++ html/search/all_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['temp_5foffset',['temp_offset',['../class_adafruit___l_p_s2_x.html#ae5bc95c5990866a61a7c4fddc81be0b7',1,'Adafruit_LPS2X']]],
   ['temp_5fscaling',['temp_scaling',['../class_adafruit___l_p_s2_x.html#a4f6e6112d537dfc8c108376f8ee63f34',1,'Adafruit_LPS2X']]],
   ['temp_5fsensor',['temp_sensor',['../class_adafruit___l_p_s2_x.html#a8898ee75ec83207786bad034dabf3fbd',1,'Adafruit_LPS2X']]],
   ['threshp_5freg',['threshp_reg',['../class_adafruit___l_p_s2_x.html#ace39ad29e62067c754cf753b0421e8f1',1,'Adafruit_LPS2X']]]
