f9ac862c50f990de28551d9a3d6c7ab239938bd8
Doxygen CI <ci-arduino@invalid>
2022-05-04 14:24:52 +0000

    Deploy docs to GitHub Pages from commit a5725982ce
    Commit: a5725982ce83bc50c8d3be585a3a6ca1d732fa4f
    GitHub Actions run: 2270330770

--- html/search/variables_2.js
+++ html/search/variables_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['i2c_5fdev',['i2c_dev',['../class_adafruit___l_p_s2_x.html#a198e3f5145cd5cec000527c1ae33b796',1,'Adafruit_LPS2X']]],
-  ['inc_5fspi_5fflag',['inc_spi_flag',['../class_adafruit___l_p_s2_x.html#a78449fa53f00cb82051e0e1047f12a88',1,'Adafruit_LPS2X']]]
+  ['inc_5fspi_5fflag',['inc_spi_flag',['../class_adafruit___l_p_s2_x.html#a78449fa53f00cb82051e0e1047f12a88',1,'Adafruit_LPS2X']]],
+  ['isoneshot',['isOneShot',['../class_adafruit___l_p_s2_x.html#a10faae820f3c81db94a9e81ca0e7fcb9',1,'Adafruit_LPS2X']]]
 ];
